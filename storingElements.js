01768f7568e4944375875d08a091243812e294e2
Hgamerdude <henryrafael02@icloud.com>
2026-06-17 11:50:57 -0500

    update

--- storingElements.js
+++ storingElements.js
@@ -1,25 +1,25 @@
 // CODE ALONG
 // 1. Create a variable called `painting1` that stores the html element with the class `painting-1` using querySelector.
-let painting1 = querySelector(".painting-1");
+let painting1 = document.querySelector(".painting-1");
 console.log(painting1);
 
 // CODE SOLO
 // 2. Create a variable called `painting2` that stores the html element with the class `painting-2` using querySelector.
-let painting2 = querySelector(".painting-2");
+let painting2 = document.querySelector(".painting-2");
 // 3. Create a variable called `painting3` that stores the html element with the class `painting-3` using querySelector.
-let painting3 = querySelector(".painting-3");
+let painting3 = document.querySelector(".painting-3");
 // 4. Create a variable called `painting4` that stores the html element with the class `painting-4` using querySelector.
-let painting4 = querySelector(".painting-4");
+let painting4 = document.querySelector(".painting-4");
 console.log(painting4);
 // 5. Create a variable called `title` that stores the title tag with using querySelector.
-let title = querySelector("title");
+let title = document.querySelector("title");
 console.log(title);
 
 
 
 // 6. Once tasks 1-5 are complete, uncomment the code below.
-// painting1.style.width = "500px";
-// painting2.style.animation = "mynewmove 10s infinite";
-// painting3.style.animation = "dance 10s infinite";
-// painting4.style.border = " 10px solid #0fd623";
-// title.innerHTML = "MUHAHAHA!";
+painting1.style.width = "500px";
+painting2.style.animation = "mynewmove 10s infinite";
+painting3.style.animation = "dance 10s infinite";
+painting4.style.border = " 10px solid #0fd623";
+title.innerHTML = "MUHAHAHA!";
